85c4716b66fb69edabaf8514fa9ba5e89ea6fdac
hasungkim <fantaman76@hotmail.com>
2026-03-01 22:19:06 +0900

    fix output folder, fix potential secrurity issue
    - fix output folder to 'out'
    - fix potential security with 'npm audit fix'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // 이 줄을 추가하여 정적 HTML로 빌드되도록 설정합니다.
 };
 
 export default nextConfig;
